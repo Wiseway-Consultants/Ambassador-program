d0aba49e957b65b91d782d767647209219e14ecf
LeBwa1337 <lebwa1337@gmail.com>
2025-09-22 17:02:37 +0300

    add trusted csrf origins to setting.py

--- ambassador_program/settings.py
+++ ambassador_program/settings.py
@@ -28,6 +28,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://ambassador.savefryoil.com",
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
+    "http://0.0.0.0:8001",
+]
+
+
+
 
 # Application definition
 
