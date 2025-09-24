81b5534e256000e699db78d8e5c69867da767123
LeBwa1337 <lebwa1337@gmail.com>
2025-09-24 17:31:56 +0300

    update openapi yaml file

--- user/urls.py
+++ user/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     path("auth/confirm-email/", ConfirmEmailView.as_view(), name="confirm-email"),
     path("auth/resend-confirmation/", ResendConfirmationView.as_view(), name="resend-confirm-email"),
     path("auth/reset-password/send/", SendResetPasswordView.as_view(), name="reset-password-send"),
-    path("profile/qr_codes/", QrCodeView.as_view(), name="qr-codes"),
     path("auth/reset-password/", ResetPasswordView.as_view(), name="reset-password"),
     path('token/obtain/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/qr_codes/", QrCodeView.as_view(), name="qr-codes"),
 ]
