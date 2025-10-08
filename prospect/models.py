e6536c08d3cf77aae0b2ebe26eaf3accb5a935ba
LeBwa1337 <lebwa1337@gmail.com>
2025-10-08 16:47:56 +0300

    New fields for Prospect model and migrations

--- prospect/models.py
+++ prospect/models.py
@@ -3,9 +3,16 @@ from django.conf import settings
 
 
 class Prospect(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=11)
-
+    comments = models.TextField(blank=True)
+    contact_name = models.CharField(max_length=64)
+    restaurant_organisation_name = models.CharField(max_length=64)
+    ghl_contact_id = models.CharField(max_length=32, blank=True, null=True)
+    ghl_opportunity_id = models.CharField(max_length=32, blank=True, null=True)
+    deal_completed = models.BooleanField(default=False)
     invited_by_user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
