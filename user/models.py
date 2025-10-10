a10c483955e97ec93ead4d786beacf693d6a2704
LeBwa1337 <lebwa1337@gmail.com>
2025-10-10 15:32:07 +0300

    fix createsuperuser

--- user/models.py
+++ user/models.py
@@ -52,7 +52,7 @@ class CustomUserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, phone=None, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -64,7 +64,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, phone, password, **extra_fields)
 
 
 class User(AbstractUser):
