daf9f1a78bb7d362a029a89396d9892a6ccb823a
LeBwa1337 <lebwa1337@gmail.com>
2025-10-22 17:56:08 +0300

    New fields and migrations for Commission

--- commission/models.py
+++ commission/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from prospect.models import Prospect
@@ -6,5 +7,20 @@ from prospect.models import Prospect
 class Commission(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    prospect = models.OneToOneField(Prospect, on_delete=models.CASCADE)
+    prospect = models.ForeignKey(
+        Prospect,
+        on_delete=models.CASCADE,
+        related_name='commissions',
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="commissions",
+        null=False,
+        blank=False
+    )
+    commission_tree_level = models.PositiveSmallIntegerField()
     number_of_frylows = models.IntegerField(default=0)
+    # money_amount = models.FloatField(default=0)
+    # currency = models.CharField(max_length=10, blank=True, null=True)
+
