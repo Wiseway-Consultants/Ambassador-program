6c9e471cd9f34b1994aac91ba46fc6687e1eff3c
LeBwa1337 <lebwa1337@gmail.com>
2025-10-22 19:09:24 +0300

    add /commission/ endpoint to urls

--- commission/urls.py
+++ commission/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from commission.views import CommissionClaimView
+from commission.views import CommissionClaimView, CommissionListView
 
 urlpatterns = [
     path('claim/', CommissionClaimView.as_view(), name='claim-commission'),
+    path('', CommissionListView.as_view(), name='commission-list'),
 ]
