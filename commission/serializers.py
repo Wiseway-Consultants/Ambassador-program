4c91970090f952ffcbcc7afa681fdc6222c5a46e
LeBwa1337 <lebwa1337@gmail.com>
2025-10-22 17:56:20 +0300

    Commision basic serializer

---
+++ commission/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from commission.models import Commission
+
+
+class CommissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commission
+        fields = '__all__'
