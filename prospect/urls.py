779665430a66c89e996d48918ad117c537703800
LeBwa1337 <lebwa1337@gmail.com>
2025-10-24 14:21:42 +0300

    add endpoints for prospects to handle GHL status updates, if deal_is_completed or contact is deleted

--- prospect/urls.py
+++ prospect/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from prospect.views import ProspectView, StaffProspectViewSet
+from prospect.views import ProspectView, StaffProspectViewSet, CompleteDealView, GhlWebhookView
 
 urlpatterns = [
     path('', ProspectView.as_view(), name='prospect'),
+    path('ghl/webhook/', GhlWebhookView.as_view(), name='ghl-webhook-handler'),
+    path('deal/complete/', CompleteDealView.as_view(), name='prospect-complete-deal'),
     path('sales/', StaffProspectViewSet.as_view({'get': 'list', 'post': 'create'}), name='prospect-sales'),
 ]
