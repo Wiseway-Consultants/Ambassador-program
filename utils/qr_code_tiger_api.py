cacd156d9c6a9be44c01fd11208cef3b9fb3f241
LeBwa1337 <lebwa1337@gmail.com>
2025-10-27 16:08:35 +0200

    update qr code tiger api class to create qr code bundles

--- utils/qr_code_tiger_api.py
+++ utils/qr_code_tiger_api.py
@@ -45,6 +45,40 @@ class QRCodeTigerAPI:
         return data
 
     @staticmethod
+    def prepare_dynamic_qr_code_bundle_payload(qr_url, qr_frame_text):
+        data = {
+            "qrType": "qr2",
+            "qrCategory": "url",
+            "qrUrl": qr_url,
+            "qrName": "test",
+            "qr": {
+                "color01": "rgb(250,190,20)",
+                "logo": "https://media.qrtiger.com/images/2025/09/logo-(1)_82.png",
+                "backgroundColor": "rgb(255,255,255)",
+                "colorDark": "rgb(250,190,20)",
+                "colorType": "SINGLE_COLOR",
+                "grdType": "linear",
+                "gradientType": "linear",
+                "frame": 1,
+                "frameColor": "rgb(12,86,165)",
+                "frameColor2": "#3a74c5",
+                "frameColorStyleType": "SINGLE_COLOR",
+                "frameGradientType": "linear",
+                "frameGradientStartColor": "#054080",
+                "frameGradientEndColor": "rgb(243,5,5)",
+                "frameText": qr_frame_text,
+                "eye_outer": "eyeOuter2",
+                "eye_inner": "eyeInner1",
+                "eye_color": False,
+                "size": 500,
+                "qrData": "pattern0",
+                "transparentBkg": False
+            }
+        }
+
+        return data
+
+    @staticmethod
     def prepare_static_qr_code_payload(qr_url):
         data = {
             "size": 500,
@@ -89,6 +123,14 @@ class QRCodeTigerAPI:
         )
         return response.json()["qrId"]
 
+    def create_qr_code_bundle(self, url, qr_frame_text):
+        response = requests.post(
+            self.BASE_URL + "campaign/",
+            headers=self.HEADERS,
+            json=self.prepare_dynamic_qr_code_bundle_payload(url, qr_frame_text)
+        )
+        return response.json()["qrId"]
+
     def create_static_qr_code(self, url):
         response = requests.post(
             self.BASE_URL + "qr/static",
@@ -114,6 +156,12 @@ class QRCodeTigerAPI:
 
     def create_qr_code_with_name(self, qr_url, qr_name):
         qr_code_campaign_id = self.create_qr_code(qr_url)
+        qr_code_id = self.update_qr_code(qr_code_campaign_id, qr_name)
+        self.move_qr_code_to_folder(qr_code_id)
+        return qr_code_campaign_id
+
+    def create_bundle_qr_code_with_name(self, qr_url, qr_frame_text, qr_name):
+        qr_code_campaign_id = self.create_qr_code_bundle(qr_url, qr_frame_text)
         qr_code_id = self.update_qr_code(qr_code_campaign_id, qr_name)
         self.move_qr_code_to_folder(qr_code_id)
         return qr_code_campaign_id
