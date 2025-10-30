a8a30a18e4fedc98b4630ab34b78f2daf844d8b6
LeBwa1337 <lebwa1337@gmail.com>
2025-10-30 16:47:07 +0200

    update notification's Auth user ForeighKey

--- notifications/models.py
+++ notifications/models.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-
-User = get_user_model()
+from django.conf import settings
 
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     notification_type = models.CharField(max_length=10)
