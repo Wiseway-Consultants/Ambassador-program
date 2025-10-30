b87a37a9eef7bb35d67b9b8403efa07c89be129a
LeBwa1337 <lebwa1337@gmail.com>
2025-10-30 16:39:14 +0200

    update send_notification function with creating notification in DB

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,14 +1,23 @@
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from django.db import transaction
+
+from notifications.models import Notification
 
 
 def send_notification(user_id, message, notification_type):
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        f"user_{user_id}",
-        {
-            "type": "send_notification",
-            "message": message,
-            "notification_type": notification_type
-        },
-    )
+    with transaction.atomic():
+        Notification.objects.create(
+            user_id=user_id,
+            message=message,
+            notification_type=notification_type
+        )
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"user_{user_id}",
+            {
+                "type": "send_notification",
+                "message": message,
+                "notification_type": notification_type
+            },
+        )
