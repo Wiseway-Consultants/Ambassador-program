dfb2b167b4fd5a41484587271bb13da9b4a4ff27
LeBwa1337 <lebwa1337@gmail.com>
2025-11-10 18:27:00 +0200

    add new model for push notification token

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from notifications.models import Notification
+from notifications.models import Notification, PushNotificationDeviceToken
 
 
 @admin.register(Notification)
@@ -13,4 +13,18 @@ class NotificationAdmin(admin.ModelAdmin):
     )
     list_filter = ("created_at", "user", "created_at")
     search_fields = ("user", "prospect")
+    readonly_fields = ("created_at",)
+
+
+@admin.register(PushNotificationDeviceToken)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+        "push_token",
+        "device_type"
+    )
+    list_filter = ("created_at", "user", "created_at")
+    search_fields = ("user",)
     readonly_fields = ("created_at",)
