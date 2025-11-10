346fd7f06e05e8fcc316318bc555086bff592101
LeBwa1337 <lebwa1337@gmail.com>
2025-11-10 18:26:44 +0200

    add new model for push notification token

--- notifications/models.py
+++ notifications/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 class Notification(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=64, default="Ambassador")
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     notification_type = models.CharField(max_length=10)
@@ -11,3 +12,13 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"{self.user}: {self.message}"
+
+
+class PushNotificationDeviceToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='fcmDeviceTokens')
+    push_token = models.CharField(max_length=255, unique=True)
+    device_type = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"FCM Token for: {self.user}"
