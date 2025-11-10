bfa2a0e40f5c86897029be90e676ee9a3dd451f5
LeBwa1337 <lebwa1337@gmail.com>
2025-11-10 18:29:51 +0200

    endpoint to register user's push token

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from notifications.views import NotificationView, NotificationCleanUpView
+from notifications.views import NotificationView, NotificationCleanUpView, PushNotificationDeviceView
 
 urlpatterns = [
     path('', NotificationView.as_view(), name='retrieve-notifications'),
+    path('register-device/', PushNotificationDeviceView.as_view(), name='fcm-token'),
     path('cleanup/', NotificationCleanUpView.as_view(), name='notifications-cleanup'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from notifications.models import Notification
-from notifications.serializers import NotificationSerializer
+from notifications.serializers import NotificationSerializer, PushNotificationDeviceTokenSerializer
 from ambassador_program.views import check_auth_key
 
 logger = logging.getLogger(__name__)
@@ -25,6 +25,7 @@ class NotificationView(APIView):
     def put(self, request):
         user = request.user
         notifications = Notification.objects.filter(user=user, read=False).update(read=True)
+        logger.info(f"Notification marked as read: {notifications}")
 
         return Response({"notifications_updated": notifications})
 
@@ -52,3 +53,29 @@ class NotificationCleanUpView(APIView):
             return Response({"detail": f"Successfully deleted {deleted_count} old notifications"}, status=200)
         except Exception as e:
             return Response(f"Error: {e}", status=400)
+
+
+class PushNotificationDeviceView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            logger.info(f"Request to register push notification token: {request.data}")
+
+            user = request.user
+            serializer = PushNotificationDeviceTokenSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=user)
+                logger.info(f"Successfully created push token for user: {user}")
+
+                return Response({
+                      "success": True,
+                      "message": "Device registered successfully"
+                    }, status=200)
+
+            logger.error(f"Invalid Serializer {serializer.errors}")
+            return Response({"error": f"Request data is not valid: {serializer.errors}"}, status=400)
+
+        except Exception as e:
+            logger.error(f"Push Notification Device Error: {e}")
+            return Response(f"error: {e}", status=400)
