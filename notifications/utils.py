41a88438c002bb58d2e5b7e4418bab7d073ddab6
LeBwa1337 <lebwa1337@gmail.com>
2025-11-10 18:26:03 +0200

    change notification function to have title

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,15 +1,48 @@
+import logging
+from os import getenv
+
+import requests
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from django.db import transaction
+from dotenv import load_dotenv
 
-from notifications.models import Notification
+from notifications.models import Notification, PushNotificationDeviceToken
+
+load_dotenv()
+logger = logging.getLogger(__name__)
 
 
-def send_notification(user_id, message, notification_type):
+def send_push_notification(push_token, title, message, data=None):
+    payload = {
+        'to': push_token,
+        'title': title,
+        'body': message,
+        'data': data or {},
+        'sound': 'default',
+        'priority': 'high'
+    }
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.post('https://exp.host/--/api/v2/push/send', json=payload, headers=headers)
+    logger.info(f"Push Notification sent response: {response.json()}")
+    return response.json()
+
+
+def push_notify_user(user_id, title, message):
+    tokens = PushNotificationDeviceToken.objects.filter(user_id=user_id).values_list('push_token', flat=True)
+    for token in tokens:
+        send_push_notification(token, title, message)
+
+
+def send_notification(user_id, message, notification_type, notification_title):
     with transaction.atomic():
         Notification.objects.create(
             user_id=user_id,
             message=message,
+            title=notification_title,
             notification_type=notification_type
         )
         channel_layer = get_channel_layer()
@@ -18,6 +51,8 @@ def send_notification(user_id, message, notification_type):
             {
                 "type": "send_notification",
                 "message": message,
+                "title": notification_title,
                 "notification_type": notification_type
             },
         )
+        push_notify_user(user_id, message, notification_title)
