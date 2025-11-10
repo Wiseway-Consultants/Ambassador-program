86d88fd307a2f14fd082a564bed9987394799133
LeBwa1337 <lebwa1337@gmail.com>
2025-11-10 18:27:28 +0200

    change notification function to have title

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from notifications.models import Notification
+from notifications.models import Notification, PushNotificationDeviceToken
 
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = ["id", "message", "created_at", "notification_type", "read"]
+
+
+class PushNotificationDeviceTokenSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PushNotificationDeviceToken
+        fields = ["push_token", "device_type", "created_at"]
