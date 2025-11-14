ea7af9ad62d67d3e33f75c1d3f510931b80d4ae4
LeBwa1337 <lebwa1337@gmail.com>
2025-11-14 16:08:26 +0200

    add is user superuser permission

--- prospect/permissions.py
+++ prospect/permissions.py
@@ -8,3 +8,12 @@ class IsStaffUser(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated and request.user.is_staff)
+
+
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
