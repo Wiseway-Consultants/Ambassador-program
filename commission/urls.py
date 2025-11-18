909b9a10dd6978fb1b792d713990bb2c8e959d4d
LeBwa1337 <lebwa1337@gmail.com>
2025-11-18 16:46:04 +0200

    add endpoints to create recipient and send him stripe onboarding url to email

--- commission/urls.py
+++ commission/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from commission.views import CommissionClaimView, CommissionListView
+from commission.views import (
+    CommissionClaimView,
+    CommissionListView,
+    StripeRecipientView,
+    StripeOnboardingEmailView
+)
 
 urlpatterns = [
+    path('recipients/', StripeRecipientView.as_view(), name='stripe-recipients'),
+    path('send-onboard/', StripeOnboardingEmailView.as_view(), name='stripe-onboard'),
     path('claim/', CommissionClaimView.as_view(), name='claim-commission'),
     path('', CommissionListView.as_view(), name='commission-list'),
 ]
--- commission/views.py
+++ commission/views.py
@@ -9,10 +9,12 @@ from rest_framework.views import APIView
 
 from commission.models import Commission
 from commission.serializers import CommissionListSerializer
-from commission.utlis import create_stripe_express_account
+from commission.utlis import create_stripe_express_account, create_stripe_recipient, create_bank_account_link
 from prospect.models import Prospect
+from prospect.permissions import IsSuperUser
 from prospect.utils import get_invitation_user_chain_from_prospect, get_currency_by_country_code
 from prospect.validation import validate_prospect, ValidationError
+from utils.send_email import send_email
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +24,7 @@ PERCENTAGE_COMMISSION_LEVELS = ["DIRECT SALE", 30, 20, 15, 12, 10, 8, 5]
 
 
 class CommissionClaimView(APIView):
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticated, ]
 
     def post(self, request):
 
@@ -102,3 +104,58 @@ class CommissionListView(ListAPIView):
             return Commission.objects.all()
 
         return Commission.objects.filter(user=user)
+
+
+class StripeRecipientView(APIView):
+    permission_classes = [IsSuperUser, ]
+
+    def post(self, request):
+        try:
+            data = request.data
+            commission = Commission.objects.get(id=data['id'])
+            if commission.paid:
+                return Response({"error": "Commission already paid"}, status=status.HTTP_400_BAD_REQUEST)
+            commission_recipient = commission.user
+            stripe_recipient_id = commission_recipient.stripe_account_id
+            if not stripe_recipient_id:
+
+                logger.info("Stripe recipient id not found, creating")
+                stripe_recipient_id = create_stripe_recipient(commission_recipient)
+                commission_recipient.stripe_account_id = stripe_recipient_id
+                commission_recipient.save()
+                logger.info(f"Stripe recipient account added to User: {commission_recipient.id}")
+
+            if not commission_recipient.stripe_onboard_status:
+                account_link = create_bank_account_link(stripe_recipient_id)
+                send_email(user=commission_recipient, url=account_link, email_type="stripe_onboarding")
+                logger.info("Stripe recipient email sent")
+
+            return Response({"detail": "Success, Ambassador receives an onboarding email"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(f"Error to create stripe_recipient for commission: {e}")
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StripeOnboardingEmailView(APIView):
+
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        user = request.user
+
+        recipient_account_id = user.stripe_account_id
+        if not recipient_account_id:
+            return Response({"error": "Stripe recipient account not found"}, status=status.HTTP_200_OK)
+        if user.stripe_onboard_status:
+            return Response({"error": "You already onboarded"}, status=status.HTTP_200_OK)
+        try:
+
+            account_link = create_bank_account_link(recipient_account_id)
+            logger.info(f"User: {user.id} Stripe recipient account link: {account_link}")
+            send_email(user=user, url=account_link, email_type="stripe_onboarding")
+            logger.info("Stripe recipient email sent")
+
+            return Response({"detail": "Email sent successfully"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(f"Error to send email: {e}")
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
