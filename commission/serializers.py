15c9b9b61eac0ae82b5b7c83d1771ec8ccfe3983
LeBwa1337 <lebwa1337@gmail.com>
2025-11-18 16:47:14 +0200

    Create Commission serializer for payouts

--- commission/serializers.py
+++ commission/serializers.py
@@ -12,7 +12,28 @@ class CommissionListSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "prospect",
+            "user",
             "number_of_frylows",
+            "currency",
+            "money_amount",
             "created_at",
             "updated_at",
+            "paid",
+            "stripe_transfer_id"
         ]
+        read_only_fields = ["created_at", "updated_at", "paid", "stripe_transfer_id", "money_amount", "currency"]
+
+
+class CommissionStripePayoutSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+    def validate_id(self, value):
+        try:
+            commission = Commission.objects.get(id=value)
+        except Commission.DoesNotExist:
+            raise serializers.ValidationError(f"Commission with id {value} does not exist.")
+        if commission.paid:
+            raise serializers.ValidationError(f"Commission with id {value} is already paid.")
+        if not commission.user.stripe_onboard_status:
+            raise serializers.ValidationError(f"This ambassador is not onboarded for Stripe payouts.")
+        return value
