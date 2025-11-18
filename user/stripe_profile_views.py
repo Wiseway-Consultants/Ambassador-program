d550faaaf624fb2d9c0c85f76e6a451cc781da20
LeBwa1337 <lebwa1337@gmail.com>
2025-11-18 16:21:16 +0200

    rewrite stripe profile endpoint

--- user/stripe_profile_views.py
+++ user/stripe_profile_views.py
@@ -2,14 +2,16 @@ import logging
 
 from django.conf import settings
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 import stripe
 
-from commission.utlis import create_stripe_express_account
-from utils.send_email import send_email
+from commission.models import Commission
+from commission.serializers import CommissionStripePayoutSerializer
+from commission.utlis import create_stripe_express_account, create_stripe_transfer, retrieve_recipient_stripe
+from prospect.permissions import IsSuperUser
 
 logger = logging.getLogger(__name__)
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -27,13 +29,14 @@ class StripeProfileView(APIView):
                 logger.error(f"User doesn't have stripe account")
                 return Response(
                     {
-                        "error": "You doesn't have Express Stripe account"
+                        "error": "You doesn't have Recipient Stripe account"
                     },
                     status=status.HTTP_400_BAD_REQUEST
                 )
-            account = stripe.Account.retrieve(user_stripe_acc_id)
-            if user.stripe_onboard_status is False and account.details_submitted is False:
-                user.set_stripe_onboard_status = True
+            account = retrieve_recipient_stripe(user)
+            payment_capability_status = account["configuration"]["recipient"]["capabilities"]["bank_accounts"]["local"]["status"]
+            if user.stripe_onboard_status is False and payment_capability_status == "active":
+                user.stripe_onboard_status = True
                 user.save()
                 logger.info(f"Stripe account {user_stripe_acc_id} is onboard")
             logger.debug(f"Stripe Account retrieved: {account}")
@@ -42,72 +45,24 @@ class StripeProfileView(APIView):
             logger.error(f"Error getting stripe_profile: {e}")
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request):
-
-        try:
-            user = request.user
-            if not user.stripe_account_id:
-                stripe_account = create_stripe_express_account(user=user)
-                return Response({"stripe_id": stripe_account}, status=status.HTTP_201_CREATED)
-            return Response(
-                {"stripe_id": user.stripe_account_id, "message": "Account already exists"},
-                status=status.HTTP_200_OK
-            )
-        except Exception as e:
-            logger.error(f"Error creating stripe profile: {e}")
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
 
 class StripePayoutsView(APIView):
 
-    permission_classes = (IsAdminUser, )
-
-    def get(self, request):
-        pass
-
-
-class StripeAccountView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        try:
-            user = request.user
-            logger.info(f"Request to create login link to stripe account: {user.email}")
-            user_stripe_acc_id = user.stripe_account_id
-            if not user_stripe_acc_id:
-                logger.error(f"User doesn't have stripe account")
-                return Response(
-                    {
-                        "error": "You doesn't have Express Stripe account"
-                    },
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            login_link = stripe.Account.create_login_link(user_stripe_acc_id)
-            return Response({"login_url": login_link.url}, status=status.HTTP_201_CREATED)
-        except Exception as e:
-            logger.error(f"Error creating stripe account login link: {e}")
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class StripeOnboardingView(APIView):
-
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsSuperUser, )
 
     def post(self, request):
-        user = request.user
-        user_stripe_acc_id = user.stripe_account_id
-        if not user_stripe_acc_id:
-            return Response({"error": "You don't have stripe account"}, status=status.HTTP_400_BAD_REQUEST)
 
-        account_link = stripe.AccountLink.create(
-            account=user_stripe_acc_id,  # The Express account ID
-            refresh_url="https://savefryoil.com/ambassador-referrals/stripe-profile/",  # If user abandons onboarding
-            return_url="https://savefryoil.com/ambassador-referrals/stripe-profile/",  # After onboarding completes
-            type="account_onboarding"
-        )
-        send_email(
-            user=user,
-            url=account_link.get("url"),
-            email_type="stripe_onboarding"
-        )
-        return Response({"account_link": account_link}, status=status.HTTP_200_OK)
+        data = request.data
+        serializer = CommissionStripePayoutSerializer(data=data)
+        if not serializer.is_valid():
+            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        commission_id = serializer.validated_data["id"]
+
+        try:
+            commissions = Commission.objects.get(id=commission_id)
+            user = commissions.user
+            transfer = create_stripe_transfer(user, commissions)
+            return Response({"data": transfer})
+        except Exception as e:
+            logger.error(f"Error creating stripe payout: {e}")
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- user/stripe_urls.py
+++ user/stripe_urls.py
@@ -1,15 +1,5 @@
 from django.urls import path
-
-from user.stripe_profile_views import (
-    StripeProfileView,
-    StripeOnboardingView,
-    StripeAccountView,
-    StripePayoutsView
-)
-
+from user.stripe_profile_views import StripeProfileView
 urlpatterns = [
-    path('profile/', StripeProfileView.as_view(), name='stripe-profile'),
-    path('profile/payouts/', StripePayoutsView.as_view(), name='stripe-payouts'),
-    path('profile/login-link/', StripeAccountView.as_view(), name='stripe-login-link'),
-    path('onboarding/send/', StripeOnboardingView.as_view(), name='stripe-onboarding'),
+    path('profile/', StripeProfileView.as_view(), name='stripe'),
 ]
