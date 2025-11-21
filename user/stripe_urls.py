0893353d2caebc3176a577fc7bcdbcfb849fe182
LeBwa1337 <lebwa1337@gmail.com>
2025-11-21 13:54:35 +0200

    add endpoint to urls for StripeAccountUpdateEmailView

--- user/stripe_urls.py
+++ user/stripe_urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from user.stripe_profile_views import (
     StripeProfileView,
-    StripeOnboardingEmailView, StripePayoutsView
+    StripeOnboardingEmailView,
+    StripePayoutsView,
+    StripeAccountUpdateEmailView
 )
 
 urlpatterns = [
     path('profile/', StripeProfileView.as_view(), name='stripe'),
     path('onboard/', StripeOnboardingEmailView.as_view(), name='stripe_onboard'),
+    path('account_update/', StripeAccountUpdateEmailView.as_view(), name='stripe_account_update'),
     path('payout/', StripePayoutsView.as_view(), name='stripe_payouts'),
 ]
