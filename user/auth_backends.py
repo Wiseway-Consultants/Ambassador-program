a729bb48797e55f0fb195f5660330b6022eb0cdc
lebwa <lebwa1337@gmail.com>
2026-03-18 14:20:13 +0200

    add verify_ambassador_login_salt function to auth_backends files to verify Salt login secret

--- user/auth_backends.py
+++ user/auth_backends.py
@@ -1,6 +1,15 @@
+from os import getenv
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+from dotenv import load_dotenv
+import hmac
+import hashlib
+import time
 
+load_dotenv()
+SALT_LOGIN_SECRET = getenv("SALT_LOGIN_SECRET")
+TOKEN_MAX_AGE_SECONDS = 30
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, **kwargs):
@@ -15,3 +24,24 @@ class EmailBackend(ModelBackend):
         except user_model.DoesNotExist:
             return None
         return None
+
+
+def verify_ambassador_login_salt(email: str, ts: str, token: str) -> bool:
+    try:
+        issued_at = int(ts)
+    except ValueError:
+        print("Invalid timestamp")
+        return False
+
+    if abs(time.time() - issued_at) > TOKEN_MAX_AGE_SECONDS:
+        print("Token expired")
+        return False  # expired or invalid date
+
+    message = f"{email}:{ts}"
+    print(f"Message: {message}")
+    expected = hmac.new(
+        SALT_LOGIN_SECRET.encode(), message.encode(), hashlib.sha256
+    ).hexdigest()
+    print(f"Expected: {expected}")
+
+    return hmac.compare_digest(expected, token)
