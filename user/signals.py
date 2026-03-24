45be324894a250e71d05be5f4a1fc17b055e0692
lebwa <lebwa1337@gmail.com>
2026-03-24 13:23:57 +0200

    Rework post user save signal to send user to Mailchimp only if active

--- user/signals.py
+++ user/signals.py
@@ -15,7 +15,9 @@ def trigger_new_user_logic(sender, instance, created, **kwargs):
     This function runs EVERY time a User is saved.
     The 'created' boolean tells us if it's a brand new user or just an update.
     """
-    if created and "test" not in str(instance.email):
+    if instance.is_active and not instance.in_mail_chimp and "test" not in str(instance.email):
         logger.info("New user created: ", instance.id)
         mailchimp_api.add_contact_to_audience(instance)
         logger.debug("User added to Mailchimp")
+        instance.in_mail_chimp = True
+        instance.save()
