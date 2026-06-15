df42cc3f7ef6cbea554f474454d1f9ac3c97c14c
vladysavsabo <lebwa1337@gmail.com>
2026-06-15 16:40:03 +0300

    remove GHL refresher agency token from schedule tasks

--- schedule_tasks.py
+++ schedule_tasks.py
@@ -10,12 +10,6 @@ from utils.ghl_api import GHL_API
 load_dotenv()
 SERVER_TYPE = getenv("SERVER_TYPE")
 
-
-def refresher():
-    print("START REFRESHING PROCESS")
-    GHL_API.refresh_agency_token()  # GHL access token
-
-
 def cleanup_notifications():
     time.sleep(10)  # 10 seconds delay to wait server to start
     requests.get(
@@ -27,9 +21,7 @@ def cleanup_notifications():
 
 
 print("Initialize schedule tasks")
-refresher()
 cleanup_notifications()
-schedule.every(50).minutes.do(refresher)
 schedule.every().day.do(cleanup_notifications)
 # schedule.every(10).seconds.do(refresher)
 
